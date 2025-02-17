038f477638226c618d86688e11d8c9a34631a3c3
sumitkshaw <shawsumit6286@gmail.com>
2025-02-18 03:24:43 +0530

    first commit

---
+++ src/test/java/com/sumitshaww/snipLytix/SnipLytixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumitshaww.snipLytix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipLytixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
